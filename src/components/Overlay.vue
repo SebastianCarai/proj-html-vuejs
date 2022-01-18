d44038a3e43e317d9de36c1d28ee763a8c21e2a4
SebastianCarai <sebastiancarai7@gmail.com>
2022-01-18 18:19:29 +0100

    overlay

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -19,7 +19,20 @@ export default {
 <style lang="scss" scoped>
 @import '../style/general.scss';
 @import '../style/variables.scss';
-
+// Overlay
+.overlay{
+    color: white;
+    font-weight: 600;
+    text-align: center;
+    font-size: 25px;
+    display: none;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(rgba(252,117,35,0.5), rgba(252,117,35,1));
+}
 .paperclip{
         height: 50px;
         width: 50px;
