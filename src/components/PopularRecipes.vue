b2b4020fc144650507f2377b4b84b8ae33871b1e
SebastianCarai <sebastiancarai7@gmail.com>
2022-01-18 10:23:39 +0100

    poluar recipes

--- src/components/PopularRecipes.vue
+++ src/components/PopularRecipes.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="container_65 popular_recipes mt-4">
+        <!-- Intro titles -->
         <div class="my-5">
             <h3 class="section_title mt-2 mb-4">POPULAR RECIPES</h3>
             <p class="font_m">Aliquam erat volupat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non nunc lacinia lobortis</p>
         </div>
 
+        <!-- Main content -->
         <div class="d-flex justify-content-between">
             <div class="my_half_col">
                 <Card100 v-for="(cardInfo, index) in popularRecipesArray" :key="index" :card100Info="cardInfo"  />
                 <div class="orange_btn mx-4"> LEARN MORE </div>
             </div>
-            <div class="my_half_col">
-                
+            <div class="my_half_col d-flex row-cols-2 flex-wrap">
+                <div class="px-3 pb-4" v-for="(imagePath,index) in imgsArray" :key="index"><img :src="require(`../assets/img/${imagePath}`)" alt=""></div>
             </div>
         </div>
     </div>
@@ -34,6 +36,16 @@ export default {
                     cardCredits: 'Bakery, Featured, Healthy, Latest Recipes, Staff Picks',
                     cardDescription: 'Suspendisse at semper odio. Nam fringilla scelerisque tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec tincidunt posuere ornare. Phasellus placerat odio non feugiat sollicitudin. Integer vitae elementum ex. Sed porttitor, diam eget convallis volutpat, arcu tellus facilisis nulla, id dignissim orci leo id'
                 }
+            ],
+            imgsArray:[
+                'Mixed-fruits-1200x790.jpg',
+                'r-rachel-park-366508-unsplash-min-1200x790.jpg',
+                'r-michelle-tsang-500721-unsplash-min-1200x790.jpg',
+                'quick-summer-drink-460x295.jpg',
+                'r-maarten-van-den-heuvel-400626-unsplash-min-460x295.jpg',
+                'perfect-cosmopolitan-460x295.jpg',
+                'fi2x-6-460x295.jpg',
+                'r-brooke-lark-96398-unsplash-min-460x295.jpg',
             ]
         }   
     }
@@ -52,11 +64,12 @@ export default {
         color: lighten($main_text_color, 20%);
     }
     .my_half_col{
-        width: calc((100% / 2) - 30px);
-        background-color: pink;
-        height: 100px;
+        width: calc((100% / 2) - 20px);
         .orange_btn{
             border-radius: 2px;
+        }
+        img{
+            width: 100%;
         }
     }
 }
